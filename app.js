98f1b5081de23ae2ff743c5e52399acdd7c6daf5
Gabriel-Oficina-Bogota <gabrielarispe24@gmail.com>
2021-04-24 17:32:58 -0500

    testr

--- app.js
+++ app.js
@@ -9,29 +9,13 @@ require("dotenv").config();
 app.use(express.static("public"));
 
 
-
-
-app.get('/Contacto', function (req, res) {
-
-    //res.send('PAGINA NO ENCONTRADA');
-    res.redirect('http://localhost:8081/Contacto');
-    
-    res.end();
-});
-
-
-
-
 app.get('*', function (req, res) {
 
-    //res.send('PAGINA NO ENCONTRADA');
-    res.redirect('http://localhost:8081/');
 
-    res.end();
+    res.sendFile(__dirname + "/Public/index.html");
+
+    //res.end();
+
 });
-
-
-
-
 
 app.listen(process.env.PORT);
